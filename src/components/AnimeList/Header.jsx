ab6e0157872a7537a2948cc8e4964fa6b5551b0c
bagas aditya <bagasaditya599@gmail.com>
2024-10-10 00:41:22 +0700

    com

--- src/components/AnimeList/Header.jsx
+++ src/components/AnimeList/Header.jsx
@@ -2,8 +2,10 @@ import Link from "next/link";
 
 const Header = ({ title, linkHref, linkTitle }) => {
   return (
-    <div className="flex justify-between p-4 items-center">
-      <h1 className="text-2xl p-4 font-bold text-color-primary">{title}</h1>
+    <div className="flex justify-between py-1 px-4 items-center">
+      <h1 className="text-2xl px-2 py-4 font-bold text-color-primary">
+        {title}
+      </h1>
       {linkHref && linkTitle ? (
         <Link
           href={linkHref}
